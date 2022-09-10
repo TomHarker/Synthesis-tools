d16a1cc333674f3b0fef46436c67eae9f8737da6
Thomas Harker <TomHarker@users.noreply.github.com>
2022-09-10 17:51:36 +0100

    Add files via upload
    spectrogram.py creates a .png visualisation file of an FFT performed on a .wav file

---
+++ spectrogram.py
@@ -0,0 +1,27 @@
+from scipy import signal
+from scipy.io import wavfile
+from matplotlib import pyplot as plt
+import numpy as np
+import io
+from PIL import Image
+
+# open .wav audio file
+rate, audio = wavfile.read('./inputwav')
+
+# convert to mono
+audio = np.mean(audio, axis=1)
+
+# apply FFT
+freqs, times, Sx = signal.spectrogram(audio, fs=rate, window='hann', 
+                                      nperseg=1024, noverlap=100,
+                                      detrend=False, scaling='spectrum')
+
+# switch off interactive mode and axis
+plt.ioff()
+plt.axis('off')
+
+# render and save image as .png
+plt.pcolormesh(10 * np.log10(Sx))
+img_buf = io.BytesIO()
+plt.savefig(img_buf, bbox_inches='tight', pad_inches = 0, format='png')
+im = Image.open(img_buf).save('output.png')
